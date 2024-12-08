a10bf3bd60e483f9b6883240247ac609b3387b1f
Borys G. <borysgalach529@gmail.com>
2024-12-08 11:24:43 +0100

    jk not final

--- js/file1.js
+++ js/file1.js
@@ -4,16 +4,19 @@ function changeThemeOnStart() {
 }
 
 function changeTheme(){
+
+    var root = document.querySelector(':root');
+
     if ( document.body.classList.contains('dark') ){
         document.body.classList.toggle('light');
-        document.documentElement.style.cssText = "--mid-color: #797979"; //LIGHT MID COLOR
-        document.documentElement.style.cssText =  "--darker-mid-color: #919292"; //LIGHT DARKER MID COLOR
+        root.documentElement.style.setProperty("--mid-color: #797979"); //LIGHT MID COLOR
+        root.documentElement.style.setProperty("--darker-mid-color: #919292"); //LIGHT DARKER MID COLOR
     }
     else if (document.body.classList.contains('light'))
     {
         //dark mode changes
         document.body.classList.toggle('dark');
-        document.documentElement.style.cssText = "--mid-color: #868686"; //DARK MID COLOR
-        document.documentElement.style.cssText =  "--darker-mid-color: #6e6d6d"; //DARK DARKER MID COLOR
+        root.documentElement.style.setProperty("--mid-color: #868686"); //DARK MID COLOR
+        root.documentElement.style.setProperty("--darker-mid-color: #6e6d6d"); //DARK DARKER MID COLOR
     }
 }
