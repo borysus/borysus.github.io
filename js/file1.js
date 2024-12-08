4dcc799fcaa9e18d2171cde22867cf4dec33cec8
Borys G. <borysgalach529@gmail.com>
2024-12-08 11:34:42 +0100

    im way to bored for this shi

--- js/file1.js
+++ js/file1.js
@@ -9,14 +9,14 @@ function changeTheme(){
 
     if ( document.body.classList.contains('dark') ){
         document.body.classList.toggle('light');
-        root.documentElement.style.setProperty("--mid-color: #797979"); //LIGHT MID COLOR
-        root.documentElement.style.setProperty("--darker-mid-color: #919292"); //LIGHT DARKER MID COLOR
+        document.root.style.setProperty('--mid-color: #797979'); //LIGHT MID COLOR
+        document.root.style.setProperty('--darker-mid-color: #919292'); //LIGHT DARKER MID COLOR
     }
     else if (document.body.classList.contains('light'))
     {
         //dark mode changes
         document.body.classList.toggle('dark');
-        root.documentElement.style.setProperty("--mid-color: #868686"); //DARK MID COLOR
-        root.documentElement.style.setProperty("--darker-mid-color: #6e6d6d"); //DARK DARKER MID COLOR
+        document.root.style.setProperty('--mid-color: #868686'); //DARK MID COLOR
+        document.root.style.setProperty('--darker-mid-color: 6e6d6d'); //DARK DARKER MID COLOR
     }
 }
