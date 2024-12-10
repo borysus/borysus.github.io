baf06e456bee81f95d939d891203d0aeda797ba3
Borys G. <borysgalach529@gmail.com>
2024-12-10 22:41:30 +0100

    name

--- js/file1.js
+++ js/file1.js
@@ -4,17 +4,11 @@ function changeThemeOnStart() {
 }
 
 function changeTheme(){
-
-    // scrollbarThumb = document.querySelector(':webkit-scrollbar-thumb'); 
-    let links = document.querySelectorAll('a'); // Select all <a> elements
-
     if ( document.body.classList.contains('dark') ){
         document.body.classList.replace('dark', 'light');
-        links.forEach(link.classList.replace('adark', 'alight'));
     }
     else if (document.body.classList.contains('light'))
     {
         document.body.classList.replace('light', 'dark');
-        links.forEach(link => link.classList.replace('alight', 'adark'));
     }
 }
