b1ff5b1dc4d5032408645d1002b957d1da30a2c8
Borys G. <borysgalach529@gmail.com>
2024-12-10 22:08:39 +0100

    idk man

--- js/file1.js
+++ js/file1.js
@@ -5,11 +5,11 @@ function changeThemeOnStart() {
 
 function changeTheme(){
 
-    // scrollbarThumb = document.querySelector(':webkit-scrollbar-thumb');
+    // scrollbarThumb = document.querySelector(':webkit-scrollbar-thumb'); 
 
     if ( document.body.classList.contains('dark') ){
         document.body.classList.toggle('light');
-        document.querySelector('a').classList.toggle('a-light');
+        body.a.classList.toggle('a-light');
         // document.scrollbarThumb.classList.toggle('webkit-scrollbar-thumb-light')
         // i gave up on root.style.setProperty('--mid-color', '#797979'); //LIGHT MID COLOR
         // root.style.setProperty('--darker-mid-color', '#919292'); //LIGHT DARKER MID COLOR
@@ -17,7 +17,7 @@ function changeTheme(){
     else if (document.body.classList.contains('light'))
     {
         document.body.classList.toggle('dark');
-        document.querySelector('a').classList.toggle('a-dark');
+        body.a.classList.toggle('a-dark');
         // document.scrollbarThumb.classList.toggle('webkit-scrollbar-thumb-dark')
         // root.style.setProperty('--mid-color', '#868686'); //DARK MID COLOR
         // root.style.setProperty('--darker-mid-color', '6e6d6d'); //DARK DARKER MID COLOR
