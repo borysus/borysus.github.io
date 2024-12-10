39479b47a437d0a25a787893eb6cba0bbce7f5b7
Borys G. <borysgalach529@gmail.com>
2024-12-10 22:32:09 +0100

    nwm

--- js/file1.js
+++ js/file1.js
@@ -6,20 +6,15 @@ function changeThemeOnStart() {
 function changeTheme(){
 
     // scrollbarThumb = document.querySelector(':webkit-scrollbar-thumb'); 
+    let links = document.querySelectorAll('a'); // Select all <a> elements
 
     if ( document.body.classList.contains('dark') ){
-        document.body.classList.toggle('light');
-        body.a.classList.toggle('a-light');
-        // document.scrollbarThumb.classList.toggle('webkit-scrollbar-thumb-light')
-        // i gave up on root.style.setProperty('--mid-color', '#797979'); //LIGHT MID COLOR
-        // root.style.setProperty('--darker-mid-color', '#919292'); //LIGHT DARKER MID COLOR
+        document.body.classList.replace('dark', 'light');
+        links.forEach(link.classList.replace('adark', 'alight'));
     }
     else if (document.body.classList.contains('light'))
     {
-        document.body.classList.toggle('dark');
-        body.a.classList.toggle('a-dark');
-        // document.scrollbarThumb.classList.toggle('webkit-scrollbar-thumb-dark')
-        // root.style.setProperty('--mid-color', '#868686'); //DARK MID COLOR
-        // root.style.setProperty('--darker-mid-color', '6e6d6d'); //DARK DARKER MID COLOR
+        document.body.classList.replace('light', 'dark');
+        links.forEach(link => link.classList.replace('alight', 'adark'));
     }
 }
