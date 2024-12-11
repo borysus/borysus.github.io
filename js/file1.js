98b2d7ce5ff2c7d6a5f8284391a2a0d7de37e91a
Borys G. <borysgalach529@gmail.com>
2024-12-11 19:33:38 +0100

    yyyyyyyyyyyy ttyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- js/file1.js
+++ js/file1.js
@@ -9,15 +9,14 @@ function changeTheme() {
     const links = document.querySelectorAll('.link');
     
     if (document.body.classList.contains('dark')) {
-        document.body.classList.remove('dark');
-        document.body.classList.add('light');
+        document.body.classList.toggle('light');
         links.forEach(link => {
             link.classList.remove('adark');
             link.classList.add('alight');
         });
-    } else {
-        document.body.classList.remove('light');
-        document.body.classList.add('dark');
+    } 
+    else if (document.body.classList.contains('light')) {
+        document.body.classList.toggle('dark');
         links.forEach(link => {
             link.classList.remove('alight');
             link.classList.add('adark');
