beb4f4f4bb32a20c44db5051f8527a794ef069ae
Borys G. <borysgalach529@gmail.com>
2024-12-11 18:29:11 +0100

    dam idk what am i even doing man e

--- js/file1.js
+++ js/file1.js
@@ -4,34 +4,24 @@ function changeThemeOnStart() {
     document.getElementsByClassName('link').classList.toggle('adark');
 }
 
-//function changeTheme(){
-    //if ( document.body.classList.contains('dark') ){
-        //document.body.classList.toggle('light');
-        //document.getElementsByClassName('link').classList.toggle('alight');
-    //}
-    //else if (document.body.classList.contains('light'))
-    //{
-        //document.body.classList.toggle('dark');
-        //document.getElementsByClassName('link').classList.toggle('adark');
-    //}
-//}
 
 function changeTheme() {
-    const links = document.getElementsByClassName('link');
+    const links = document.querySelectorAll('.link');
     
     if (document.body.classList.contains('dark')) {
         document.body.classList.remove('dark');
         document.body.classList.add('light');
-        for (let i = 0; i < links.length; i++) {
-            links[i].classList.remove('adark');
-            links[i].classList.add('alight');
-        }
+        links.forEach(link => {
+            link.classList.remove('adark');
+            link.classList.add('alight');
+        });
     } else {
         document.body.classList.remove('light');
         document.body.classList.add('dark');
-        for (let i = 0; i < links.length; i++) {
-            links[i].classList.remove('alight');
-            links[i].classList.add('adark');
-        }
+        links.forEach(link => {
+            link.classList.remove('alight');
+            link.classList.add('adark');
+        });
     }
 }
+
