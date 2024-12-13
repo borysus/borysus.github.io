a65e28d6957fcf1ada87d13d931a718f0ff06327
Borys G. <borysgalach529@gmail.com>
2024-12-13 17:29:34 +0100

    nwm cos

--- js/file1.js
+++ js/file1.js
@@ -1,21 +1,25 @@
 function changeThemeOnStart() {
     var element = document.body;
-    element.classList.toggle("dark");
-    document.getElementsByClassName('link').classList.toggle('adark');
-}
-
-
-function changeTheme() {
-    const links = document.querySelectorAll('.link');
-    
-    if (document.body.classList.contains('dark')) {
-        document.body.classList.toggle('light');
-        links.classList.toggle('alight');
-    } 
-    else if (document.body.classList.contains('light')) {
-        document.body.classList.toggle('dark');
-        
-        links.classList.toggle('adark');
+    var links = document.getElementsByClassName('link');
+    element.classList.add('dark');
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.add('alight');
+    }
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.toggle('alight');
     }
 }
 
+function changeTheme() {
+    var links = document.getElementsByClassName('link');
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.add('light');
+        document.body.classList.remove('dark');
+    } else if (document.body.classList.contains('light')) {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+    }
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.toggle('alight');
+    }
+}
