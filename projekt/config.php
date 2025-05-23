ad1c852cf0a840e77c2429c39780ce588feec41b
Borys G. <129661711+borysus@users.noreply.github.com>
2025-05-23 17:54:32 +0200

    Delete projekt directory

--- projekt/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nrSpotkania = 23;
-$naglowek = "Spotkanie nr $nrSpotkania";
-$title = "Projekt";
-
-$dbHost = 'localhost';
-$dbUser = 'root';
-$dbPassword = '';
-$dbName = 'projekt';
--- projekt/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('functions.php');
-unset($_SESSION['adminId']);
-header('Location: index.php');
-exit();
